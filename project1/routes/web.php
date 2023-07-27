a0f897f8a98617572d5a4659b968473597fef364
syedakkeal26 <syedakkeal@colanonline.com>
2023-07-27 11:14:25 +0530

    Update web.php

--- project1/routes/web.php
+++ project1/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome',['name'=>'Syed Akkeal']);
 });
+
+Route::view('second', 'second');
+Route::view('user', 'third');
+Route::view('company', 'company');
+Route::view('contact', 'contact');
+Route::view('about', 'about');
+Route::view('service', 'service');
+Route::view('career', 'career',['name'=> 'Syed AKkeal']);
