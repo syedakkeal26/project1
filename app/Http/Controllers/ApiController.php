43992b94c742797c478e303f18bc58033dd83d4a
syedakkeal26 <syedakkeal@colanonline.com>
2023-10-23 20:10:01 +0530

    Final commit

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -69,41 +69,7 @@ class ApiController extends Controller
 
     // }
 
-    public function profile_update(Request $request)
-        {
-            $validator = Validator::make($request->all(), [
-                        'name' => 'required',
-                        'profile_picture' => 'required|nullable|image|mimes:jpg,jpeg,png',
-                        'gender' => 'required|nullable',
-                    ]);
-            if ($validator->fails()) {
-                        $error = $validator->errors()->all();
-                        return response()->json(['message' => $error], 400);
-                    }
-            $user = $request->user();
-            if ($request->hasFile('profile_picture')) {
-                if ($user->profile_picture) {
-                    $old_path = public_path('/uploads/profile_images/' . $user->profile_picture);
-                    if (File::exists($old_path)) {
-                        File::delete($old_path);
-                    }
-                }
-                $image = 'profile_picture_' . time() . '.' . $request->file('profile_picture')->extension();
-                $destinationPath = public_path('/uploads/profile_images');
-
-                if (!$request->file('profile_picture')->move($destinationPath, $image)) {
-                    return response()->json(['message' => 'File not moved. Check directory permissions or other issues.'], 400);
-                }
-            } else {
-                $image = $user->profile_picture;
-            }
-            $user->update([
-                'name' => $request->input('name'),
-                'gender' => $request->input('gender'),
-                'profile_picture' => $image,
-            ]);
-            return response()->json(['message' => 'Profile Updated Successfully']);
-        }
+ 
         public function changePassword(Request $request)
     {
         $validator = Validator::make($request->all(), [
